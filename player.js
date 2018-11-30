f130552333c2adef65ae1fd38e6afa1896b0e514
zloid <zloid_1@mail.ru>
2018-11-30 21:15:25 +0100

    UI //refactoring

--- player.js
+++ player.js
@@ -34,13 +34,13 @@ isFirstPlayBool = true,
 muteSwitchBool = false,
 randomSwitchBool = false,
 arrayAllHTMLAudioElementObj = document.querySelectorAll('audio'),
-stopButtonObj = document.querySelector('#stop'),
-playButtonObj = document.querySelector('#play'),
-nextButtonObj = document.querySelector('#next'),
-backButtonObj = document.querySelector('#back'),
+// stopButtonObj = document.querySelector('#stop'),
+// playButtonObj = document.querySelector('#play'),
+// nextButtonObj = document.querySelector('#next'),
+// backButtonObj = document.querySelector('#back'),
 muteButtonObj = document.querySelector('#mute'),
 randomButtonObj = document.querySelector('#random'),
-takeAudioProgressBarObj = document.querySelector('#songSlider'),
+// takeAudioProgressBarObj = document.querySelector('#songSlider'),
 arrayOfNewUIAudioInterfaceObj;
 
 
@@ -48,6 +48,10 @@ arrayOfNewUIAudioInterfaceObj;
 
 function drawNewUi (arrayAllAudio, setNewClassForNewUIAudio, putNewUIThere, displayOldUIAudio, oldUIAudioControlsBool) {
     let containerForNewUIAudioElement = document.querySelector(putNewUIThere);
+    //todo
+    drawProgressBarScreen(containerForNewUIAudioElement);
+    drawAllNewButtons(containerForNewUIAudioElement);
+
     let newUl = document.createElement('ul');        
     for (let i = 0; i < arrayAllAudio.length; i++) {                 
         arrayAllAudio[i].style.display = displayOldUIAudio;        
@@ -68,7 +72,60 @@ function drawNewUi (arrayAllAudio, setNewClassForNewUIAudio, putNewUIThere, disp
     }    
     containerForNewUIAudioElement.appendChild(newUl);
 }
+ 
+//  ===================
+function drawAllNewButtons (containerForNewUIAudioElement) {
+    let divForButtons = document.createElement('div');
+    divForButtons.id = 'allButtonsHere';    
+    containerForNewUIAudioElement.appendChild(divForButtons);
+    //stop
+    let newBtnStop = document.createElement('button');
+    newBtnStop.id = 'stop';
+    newBtnStop.innerHTML = 'STOP';
+    //play
+    let newBtnPlay = document.createElement('button');
+    newBtnPlay.id = 'play';
+    newBtnPlay.innerHTML = 'PLAY';
+    //back
+    let newBtnBack = document.createElement('button');
+    newBtnBack.id = 'back';
+    newBtnBack.innerHTML = 'BACK';
+    //next
+    let newBtnNext = document.createElement('button');
+    newBtnNext.id = 'next';
+    newBtnNext.innerHTML = 'NEXT';
+    //render
+    divForButtons.appendChild(newBtnStop);
+    divForButtons.appendChild(newBtnBack);
+    divForButtons.appendChild(newBtnPlay);
+    divForButtons.appendChild(newBtnNext);
+}
 
+
+function drawProgressBarScreen (containerForNewUIAudioElement) {
+    let divForProgressBar = document.createElement('div');
+    divForProgressBar.id = 'progressBarContainer';
+    containerForNewUIAudioElement.appendChild(divForProgressBar);
+    //songSlider
+    let songSliderObj = document.createElement('div');
+    songSliderObj.id = 'songSlider';
+    //trackProgress
+    let trackProgressObj = document.createElement('div');
+    trackProgressObj.id = 'trackProgress';
+    //trackNameContainer
+    let trackNameContainerObj = document.createElement('div');
+    trackNameContainerObj.id = 'trackNameContainer';
+    //timer
+    let timerObj = document.createElement('div');
+    timerObj.id = 'timer';
+    timerObj.innerHTML = '0:00 &#128337; 0:00';
+    //render
+    divForProgressBar.appendChild(songSliderObj);
+    divForProgressBar.appendChild(timerObj);
+    songSliderObj.appendChild(trackProgressObj);
+    trackProgressObj.appendChild(trackNameContainerObj);
+}
+//  ===================
 
 //todo
 function playOneAudioPlus (indexOfAudio) {
@@ -124,16 +181,20 @@ function namingProgressBarEqualCurrentAudioSrc (putAudioLowerCaseNameTo, arrayAl
         let AudioLowerCaseNameContainer = document.querySelector(putAudioLowerCaseNameTo);
         let  pathToAudio = arrayAllHTMLAudioElementObj[indexOfAudio].currentSrc;
         AudioLowerCaseNameContainer.innerHTML = pathToAudio.match(/[%-\.\w]*$/ig).join('').replace(/mp3|ogg|[^a-z]+/ig, ' ').toLowerCase();
-        AudioLowerCaseNameContainer.innerHTML = `&nbsp;&nbsp; ${AudioLowerCaseNameContainer.innerHTML}`;
+        AudioLowerCaseNameContainer.innerHTML = `${AudioLowerCaseNameContainer.innerHTML}`;
     }, 1)
 }
 
 //  todo
 function setSongPosition(event){    
-    let songSliderWidth = takeAudioProgressBarObj.offsetWidth;    
+    let songSliderWidthInt = takeAudioProgressBarObj.offsetWidth;    
     //window.event
-    let clickLocation =  event.layerX - takeAudioProgressBarObj.offsetLeft;
-    let percentage = (clickLocation/songSliderWidth);
+    // let clickLocationInt =  event.layerX - takeAudioProgressBarObj.offsetLeft;
+    let clickLocationInt =  event.layerX;
+    console.log('clickLocationInt:', clickLocationInt)
+    console.log('songSliderWidthInt:', songSliderWidthInt)
+
+    let percentage = (clickLocationInt/songSliderWidthInt);
     //bingo
     arrayAllHTMLAudioElementObj[audioNowPlayingInt].currentTime = arrayAllHTMLAudioElementObj[audioNowPlayingInt].duration * percentage;
 }
@@ -236,6 +297,15 @@ function shufflePlay () {
 drawNewUi(arrayAllHTMLAudioElementObj, 'newLiClass', '#AudioPlayerByZloid', 'none', false);
 playingNewUIAudioElements('.newLiClass');
 
+//===========================================
+
+let stopButtonObj = document.querySelector('#stop'),
+backButtonObj = document.querySelector('#back'),
+nextButtonObj = document.querySelector('#next'),
+playButtonObj = document.querySelector('#play'),
+takeAudioProgressBarObj = document.querySelector('#songSlider');
+//===========================================
+
 //EVENTS  
 //play next song when prev. ended
 for (let i = 0; i < arrayAllHTMLAudioElementObj.length; i++) { 
@@ -296,3 +366,6 @@ randomButtonObj.addEventListener('click', () => {
 
 //todo xxx
 
+
+// let xxx = document.querySelector('#trackNameContainer');
+// console.log('trackNameContainer:', xxx.offsetWidth, 'px');
